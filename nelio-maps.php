0cdb21d7bd1a917a4e20e05edd61084f62535df1
David Aguilera <david.aguilera@neliosoftware.com>
2019-03-26 12:28:27 +0100

    Updated since version

--- nelio-maps.php
+++ nelio-maps.php
@@ -8,7 +8,7 @@
  * that starts the plugin.
  *
  * @link              https://neliosoftware.com
- * @since             1.0.1
+ * @since             1.0.0
  * @package           NELIO_MAPS
  *
  * @wordpress-plugin
